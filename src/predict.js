8e5606e72a6fdc38b38ce46ffc6531aeaad60ae1
cuteopamp <edayilmxz@outlook.com>
2023-02-25 12:07:16 +0300

    up up up

---
+++ src/predict.js
@@ -0,0 +1,63 @@
+const Result = {0:'Bug',
+             1:'Dark',
+             2:'Dragon',
+             3:'Electric',
+             4:'Fairy',
+             5:'Fighting',
+             6:'Fire',
+             7:'Flying',
+             8:'Ghost',
+             9:'Grass',
+             10:'Ground',
+             11:'Ice',
+             12:'Normal',
+             13:'Poison',
+             14:'Psychic',
+             15:'Rock',
+             16:'Steel',
+             17:'Water'};
+
+$("#image-selector").change(function () {
+	let reader = new FileReader();
+	reader.onload = function () {
+		let dataURL = reader.result;
+		$("#selected-image").attr("src", dataURL);
+		$("#prediction-list").empty();
+	}
+
+	let file = $("#image-selector").prop('files')[0];
+	reader.readAsDataURL(file);
+});
+
+let model;
+$(document).ready(async function () {
+	$('.progress-bar').show();
+	model = await tf.loadGraphModel('./model-js/model.json');
+	$('.progress-bar').hide();
+});
+
+$("#predictBtn").click(async function () {
+	let image = $('#selected-image').get(0);
+
+	let pre_image = tf.browser.fromPixels(image, 3)
+		.resizeNearestNeighbor([224, 224])
+		.expandDims()
+		.toFloat()
+		.reverse(-1);
+	let predict_result = await model.predict(pre_image).data();
+	console.log(predictions);
+	let order = Array.from(predict_result)
+		.map(function (p, i) {
+			return {
+				probability: p,
+				className: Result[i]
+			};
+		}).sort(function (a, b) {
+			return b.probability - a.probability;
+		}).slice(0, 2);
+
+	$("#list").empty();
+	order.forEach(function (p) {
+		$("#list").append(`<li>${p.className}: ${parseInt(Math.trunc(p.probability * 100))} %</li>`);
+	});
+});
